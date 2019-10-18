f40b92030b67ca4143d3af73519a6b37c61729b4
angelicalapastora <angelicalapastora@gmail.com>
2019-10-18 11:24:42 +0800

    deleted other files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,22 +1,21 @@
-"""portfolio URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+	# Basic Urls
+    path('hello/', views.hello),
+    path('<str:pet>/', views.hello_pet),
+    path('pets_age/<int:y>/', views.pets_age),
+
+    # URLs with templates
+    path('show/<str:pet>/', views.show_pet),
+    path('show/<str:loc>/home/', views.show_home),
+    path('show/<str:pet>/skills/', views.show_skills),
+    path('show/<str:pet>/food/', views.food),
+
+    path('page/contact/', views.contact),
+    path('page/home/', views.home),
+    path('page/about/', views.about),
 ]
